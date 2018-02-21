8364196db99874cba8f95890f7f95ab78de1c4cb
vigzmv <vigzmv@outlook.com>
2018-02-21 19:43:49 +0530

    +moveObjects action

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { getCanvasPosition } from './utils/formulas';
 import Canvas from './components/Canvas';
 
 class App extends Component {
+  componentDidMount() {
+    const self = this;
+    setInterval(() => {
+      self.props.moveObjects(self.canvasMousePosition);
+    }, 10);
+  }
+
+  trackMouse(event) {
+    this.canvasMousePosition = getCanvasPosition(event);
+  }
+
   render() {
-    return <Canvas />;
+    return <Canvas angle={this.props.angle} trackMouse={event => this.trackMouse(event)} />;
   }
 }
+
+App.propTypes = {
+  angle: PropTypes.number.isRequired,
+  moveObjects: PropTypes.func.isRequired,
+};
 
 export default App;
